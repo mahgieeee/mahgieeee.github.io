1f19e3793bdde2c9ba35cf2f50a6431998c97d0f
mahgieeee <10890349+mahgieeee@users.noreply.github.com>
2019-10-07 22:44:36 -0400

    logo

--- javascript/scrollbar.js
+++ javascript/scrollbar.js
@@ -1,5 +1,34 @@
 window.onscroll = function() {scrollFunction()};
-                
+
+var mq = window.matchMedia("screen and (max-device-width: 280px) and (orientation: portrait)");
+
+function scrollFunction(){
+    if (!mq.matches){
+        if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+            document.getElementById("topnav-container").style.padding = "10px 10px";
+            document.getElementById("logo").style.fontSize = "45px";
+            } else {
+            document.getElementById("topnav-container").style.padding = "40px 10px";
+            document.getElementById("logo").style.fontSize = "58px";
+            }
+        }
+    else{
+        if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+            document.getElementById("topnav-container").style.padding = "10px 10px";
+            document.getElementById("logo").style.fontSize = "15px";
+            } else {
+            document.getElementById("topnav-container").style.padding = "20px 10px";
+            document.getElementById("logo").style.fontSize = "18px";
+            }
+    }
+}
+   
+
+
+
+
+
+/*
 function scrollFunction() {
     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("topnav-container").style.padding = "10px 10px";
@@ -8,4 +37,4 @@ function scrollFunction() {
     document.getElementById("topnav-container").style.padding = "40px 10px";
     document.getElementById("logo").style.fontSize = "58px";
     }
-}
+}*/
